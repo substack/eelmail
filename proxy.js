80af9af52c7e451811b7caf7d0465e339704d700
James Halliday <mail@substack.net>
2014-07-12 13:56:54 -0700

    working yay

--- proxy.js
+++ proxy.js
@@ -1,4 +1,6 @@
 var net = require('net');
+var x256 = require('x256');
+var through = require('through2');
 var forwards = {
     25: 9025,
     143: 9143
@@ -7,11 +9,35 @@ var forwards = {
 Object.keys(forwards).forEach(function (src) {
     var dst = forwards[src];
     var server = net.createServer(function (stream) {
-console.log('CONNECTION', src); 
+        console.error(Date.now() + ' begin');
+        
+        stream.on('end', function () {
+            console.error(Date.now() + ' end');
+        });
+        
         var c = net.connect(dst);
-        c.on('error', function () {});
-        stream.on('error', function () {});
+        c.on('error', function (err) {
+            console.error(err + '');
+        });
+        stream.on('error', function () {
+            console.error(err + '');
+        });
+        
+        stream.pipe(colorize([ 63, 255, 63 ])).pipe(process.stderr);
+        c.pipe(colorize([ 63, 63, 255 ])).pipe(process.stderr);
+        
         c.pipe(stream).pipe(c);
     });
     server.listen(src);
 });
+
+function colorize (rgb) {
+    return through(function (buf, enc, next) {
+        this.push(Buffer.concat([
+            Buffer('\x1b[38;5;' + x256(rgb) + 'm'),
+            buf,
+            Buffer('\x1b[00m')
+        ]));
+        next();
+    });
+}
