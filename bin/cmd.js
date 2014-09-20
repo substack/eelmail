c3ce0e846de9bc81d1dda095f1238a85d2057c7f
James Halliday <mail@substack.net>
2014-09-20 15:00:52 +0300

    use minimist for options

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,15 +1,34 @@
 #!/usr/bin/env node
 
 var eelmail = require('../');
-var level = require('level');
-var db = level('/tmp/mail/db');
-var em = eelmail(db, { dir: '/tmp/mail/blob' });
+var level = require('level-party');
+var minimist = require('minimist');
+var path = require('path');
+var mkdirp = require('mkdirp');
+
+var argv = minimist(process.argv.slice(2), {
+    alias: { d: 'dir' },
+    default: {
+        dir: path.join(process.cwd(), 'eelmail.db'),
+        ports: { smtp: 25, imap: 143 }
+    }
+});
+mkdirp.sync(argv.dir);
+
+var db = level(path.join(argv.dir, 'db'));
+var em = eelmail(db, { dir: argv.dir });
+
+if (argv._[0] === 'manage') {
+}
 
 em.users.create('substack@localhost', {
     login: { basic: { username: 'substack', password: 'beep boop' } },
     value: { bio: 'beep boop' }
 });
 
-em.createServer('smtp').listen(9025);
-em.createServer('pop').listen(9110);
-em.createServer('imap').listen(9143);
+var servers = {
+    smtp: em.createServer('smtp'),
+    imap: em.createServer('imap')
+};
+servers.smtp.listen(argv.ports.smtp);
+servers.imap.listen(argv.ports.imap);
