efe02f21f904ac61bc7030b4c5b7618f9c47362f
James Halliday <mail@substack.net>
2014-09-21 02:04:29 +0300

    accountdown-command

--- bin/cmd.js
+++ bin/cmd.js
@@ -3,6 +3,7 @@
 var eelmail = require('../');
 var level = require('level-party');
 var minimist = require('minimist');
+var userCommand = require('accountdown-command');
 var path = require('path');
 var mkdirp = require('mkdirp');
 
@@ -18,17 +19,16 @@ mkdirp.sync(argv.dir);
 var db = level(path.join(argv.dir, 'db'));
 var em = eelmail(db, { dir: argv.dir });
 
-if (argv._[0] === 'manage') {
+if (process.argv[2] === 'users') {
+    userCommand(db, process.argv.slice(3), {
+        command: 'eelmail users'
+    });
 }
-
-em.users.create('substack@localhost', {
-    login: { basic: { username: 'substack', password: 'beep boop' } },
-    value: { bio: 'beep boop' }
-});
-
-var servers = {
-    smtp: em.createServer('smtp'),
-    imap: em.createServer('imap')
-};
-servers.smtp.listen(argv.ports.smtp);
-servers.imap.listen(argv.ports.imap);
+else {
+    var servers = {
+        smtp: em.createServer('smtp'),
+        imap: em.createServer('imap')
+    };
+    servers.smtp.listen(argv.ports.smtp);
+    servers.imap.listen(argv.ports.imap);
+}
