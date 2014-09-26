8a04d303548ee7b8d4281cf3b8ebb554923f6f1d
James Halliday <mail@substack.net>
2014-09-26 16:41:49 +0300

    account-down command, usage stub

--- bin/cmd.js
+++ bin/cmd.js
@@ -4,11 +4,12 @@ var eelmail = require('../');
 var level = require('level-party');
 var minimist = require('minimist');
 var userCommand = require('accountdown-command');
+var fs = require('fs');
 var path = require('path');
 var mkdirp = require('mkdirp');
 
 var argv = minimist(process.argv.slice(2), {
-    alias: { d: 'dir' },
+    alias: { d: 'dir', h: 'help' },
     default: {
         dir: path.join(process.cwd(), 'eelmail.db'),
         ports: { smtp: 25, imap: 143 }
@@ -20,15 +21,32 @@ var db = level(path.join(argv.dir, 'db'));
 var em = eelmail(db, { dir: argv.dir });
 
 if (process.argv[2] === 'users') {
-    userCommand(db, process.argv.slice(3), {
-        command: 'eelmail users'
-    });
+    var opts = { command: 'eelmail users' };
+    userCommand(em.users, process.argv.slice(3), opts, function (err) {
+        if (err) {
+            console.error(err + '');
+            process.exit(1);
+        }
+        db.close();
+    }).pipe(process.stdout);
 }
-else {
+else if (argv.help) {
+    showHelp(0);
+}
+else if (argv._[0] === 'server') {
     var servers = {
         smtp: em.createServer('smtp'),
         imap: em.createServer('imap')
     };
     servers.smtp.listen(argv.ports.smtp);
     servers.imap.listen(argv.ports.imap);
+}
+else showHelp(1);
+
+function showHelp (code) {
+    var r = fs.createReadStream(path.join(__dirname, 'usage.txt'));
+    if (code) r.once('end', function () {
+        process.exit(code);
+    });
+    r.pipe(process.stdout);
 }
